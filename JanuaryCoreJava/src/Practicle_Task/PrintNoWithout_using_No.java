a59c855736e0aff740c97e0266d7fd19bd35e662
Alok <alokahamana@gmail.com>
2024-05-10 19:59:03 +0530

    overloading code commited

---
+++ JanuaryCoreJava/src/Practicle_Task/PrintNoWithout_using_No.java
@@ -0,0 +1,23 @@
+package Practicle_Task;
+
+public class PrintNoWithout_using_No {
+
+	public static void main(String[] args) {
+		
+	int one= 'A'/'A';
+	String s = "..........";
+	
+	for(int i=one; i<=s.length()*s.length(); i++){
+		
+		System.out.print(i);
+	}
+		
+		//second method
+		
+		for(int j=one; j<='d'; j++) {
+			System.out.println(j);
+		}
+		
+	}
+	}	
+
---
+++ JanuaryCoreJava/src/com/aa/Oops/polymorphism/Test.java
@@ -0,0 +1,11 @@
+package com.aa.Oops.polymorphism;
+
+public class Test {
+	
+	
+	public static void main(String[] args) {
+		String s= null;
+		System.out.println(s.charAt(1));
+		
+	}
+}
