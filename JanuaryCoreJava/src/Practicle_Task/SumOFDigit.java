c90b2aa5944d5d77b9475ec8d0a4c0664b3fa7c8
Alok <alokahamana@gmail.com>
2024-05-18 18:38:15 +0530

    commited 18/05

--- JanuaryCoreJava/src/Practicle_Task/SumOFDigit.java
+++ JanuaryCoreJava/src/Practicle_Task/SumOFDigit.java
@@ -1,20 +1,21 @@
 package Practicle_Task;
 
 public class SumOFDigit {
-	
+
 	public static void main(String[] args) {
-		
-	int num =12345;
-	
-	int rem=0;
-	int sum=0;
-	
-	while(num>0) {
-		
-		rem =num%10;
-		sum= sum+rem;
-		num=num/10;
-		
+
+		int num = 12345;
+
+		int rem = 0;
+		int sum = 0;
+
+		while (num > 0) {
+
+			rem = num % 10;
+			sum = sum + rem;
+			num = num / 10;
+
+		}
+		System.out.println(sum);
 	}
-System.out.println(sum);
-}}
+}
